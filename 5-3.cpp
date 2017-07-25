43e3df213669c2f38f01c44bba5007301699942a
YuChen Yan <yanyuchen@xiyoulinux.org>
2017-07-25 11:20:19 +0800

    监听队列

---
+++ 5-3.cpp
@@ -0,0 +1,59 @@
+/*************************************************************************
+	> File Name: 5-3.c
+	> Author: yanyuchen
+	> Mail: 794990923@qq.com
+	> Created Time: 2017年07月25日 星期二 08时54分59秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<signal.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<assert.h>
+#include<string.h>
+
+static bool stop = false; //SIGTERM 信号的处理函数，出发时结束主程序中的循环
+static void handle_term(int sig)
+{
+    stop = true;
+}
+
+int main(int argc, char* argv[])
+{
+    signal(SIGTERM, handle_term);
+
+    if(argc <= 3) {
+        printf("usage: %s ip_address port_number backlog\n", basename(argv[0]));
+        return 1;
+    }
+    const char* ip = argv[1];
+    int port = atoi(argv[2]);
+    int backlog = atoi(argv[3]);
+
+    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    assert(sock >= 0);
+
+    //创建一个IPv4 socket地址
+    struct sockaddr_in address;
+    bzero(&address,sizeof(address));
+    address.sin_family = AF_INET;
+    inet_pton(AF_INET,ip, &address.sin_addr);
+    address.sin_port = htons(port);
+
+    int ret = bind(sock, (struct sockaddr*)&address, sizeof(address));
+    assert(ret != -1);
+
+    ret = listen(sock,backlog);
+    assert(ret != -1);
+
+    while (!stop) {
+        sleep(1);
+    }
+
+    //关闭socket
+    close(sock);
+    return 0;
+}

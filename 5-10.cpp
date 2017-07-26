32a558c1fa1eb1a5a138cacc67373b41eabf35c5
YuChen Yan <yanyuchen@xiyoulinux.org>
2017-07-26 16:13:58 +0800

    修改TCP发送缓冲区的客户端

---
+++ 5-10.cpp
@@ -0,0 +1,53 @@
+/*************************************************************************
+	> File Name: 5-10.cpp
+	> Author: yanyuchen
+	> Mail: 794990923@qq.com
+	> Created Time: 2017年07月26日 星期三 08时44分53秒
+ ************************************************************************/
+
+#include<iostream>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<assert.h>
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define BUFFER_SIZE 512
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    if (argc <= 2) {
+        printf("usage: %s ip_address port_number send_bufer_size\n", basename(argv[0]));
+        return 1;
+    }
+    const char* ip = argv[1];
+    int port = atoi(argv[2]);
+
+    struct sockaddr_in server_address;
+    bzero(&server_address, sizeof(server_address));
+    server_address.sin_family = AF_INET;
+    inet_pton(AF_INET, ip, &server_address.sin_addr);
+    server_address.sin_port = htons(port);
+
+    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    assert(sock >= 0);
+
+    int sendbuf = atoi(argv[3]);
+    int len = sizeof(sendbuf);
+    //设置TCP发送缓冲区大小，然后读取
+    setsockopt(sock, SOL_SOCKET, SO_SNDBUF, &sendbuf, sizeof(sendbuf));
+    getsockopt(sock, SOL_SOCKET, SO_SNDBUF, &sendbuf, (socklen_t*)&len);
+    printf("the tcp send buffer size after setting is %d\n", sendbuf); 
+
+    if (connect(sock, (struct sockaddr*)&server_address, sizeof(server_address)) != -1) {
+        char buffer[BUFFER_SIZE];
+        memset(buffer, 'a', BUFFER_SIZE);
+        send(sock, buffer, BUFFER_SIZE, 0);
+    }
+    close(sock);
+    return 0;
+}
